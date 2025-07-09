e13c911a71abb742c3c0c4f2933bfa029be4cfae
leon4rdmtg <Leonardomontenegro367@gmail.com>
2025-07-09 00:12:44 +0000

    hecho

---
+++ asdfa/multt/script.py
@@ -0,0 +1,14 @@
+# archivo: datos/multiplicacion.py
+
+# Pedir al usuario dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Multiplicar
+resultado = num1 * num2
+
+# Mostrar resultado
+print(f"{num1} x {num2} = {resultado}")
+
+
+
