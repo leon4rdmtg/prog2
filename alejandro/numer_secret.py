3a86f4d344347ddf846ba416c50e14fef481a6b6
Alejandro Zabala <44728236-alejandrorome74@users.noreply.replit.com>
2025-07-11 15:53:41 +0000

    Subo todos mis archivos de ejercicios

---
+++ alejandro/numer_secret.py
@@ -0,0 +1,13 @@
+numero_secreto = 7
+while True:
+  var = int(input("Adivina el número secreto: "))
+
+  if var < numero_secreto:
+    print("demasiado frio")
+  elif var > numero_secreto:
+      print("demasiado caliente")
+  else:
+        print("¡Acertaste! el número secreto es: ", numero_secreto )
+        break
+print("Fin del programa----Jose Alejandro Zabala Romero")
+assert numero_secreto == 7
