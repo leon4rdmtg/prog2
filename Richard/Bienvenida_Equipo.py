daf44b559408e79fbb8bc4f0008b4be98ea78638
richard0204 <44625851-hurtadorichardi@users.noreply.replit.com>
2025-07-22 23:17:17 +0000

    guardando cambio

---
+++ Richard/Bienvenida_Equipo.py
@@ -0,0 +1,3 @@
+nombres_estudiantes = ['Leonardo', 'Richard', 'Antonio', 'Junior', 'Nico', 'Mateo']
+for nombre in nombres_estudiantes:
+    print(f"¡Bienvenido al equipo, {nombre}!")
---
+++ Richard/Funciones.py
@@ -0,0 +1,21 @@
+def saludar(nombre_persona):
+  """
+  Recibe un nombre y devuelve un saludo personalizado (modificada para pruebas).
+  """
+  mensaje = f"¡Hola, {nombre_persona}! ¡Qué bueno tenerte aquí!"
+  return mensaje
+def sumar(a, b):
+  """
+  Recibe dos números y devuelve su suma.
+  """
+  return a + b
+# ===== Pruebas unitarias con assert =====
+assert saludar("Jose") == "¡Hola, Jose! ¡Qué bueno tenerte aquí!", "Error en saludo para Jose"
+assert saludar("Alejandro") == "¡Hola, Alejandro! ¡Qué bueno tenerte aquí!", "Error en saludo para Alejandro"
+assert sumar(2, 3) == 5, "Error: 2 + 3 debe ser 5"
+assert sumar(-4, 4) == 0, "Error: -4 + 4 debe ser 0"
+print(" Todas las pruebas unitarias pasaron correctamente.\n")
+# ===== Llamadas a las funciones =====
+print(saludar("Jose"))
+print(f"La suma de 7 y 8 es: {sumar(7, 8)}")
+
